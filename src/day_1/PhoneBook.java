ed94217e898372a071beb576fbc8a5da9c08f167
TH <zzxc6171@naver.com>
2022-11-21 17:28:00 +0900

    추가

---
+++ src/day_1/PhoneBook.java
@@ -0,0 +1,54 @@
+package day_1;
+
+import lombok.Data;
+
+@Data
+public class PhoneBook {
+	private String name;
+	private String company;
+	private int number;
+	
+	public PhoneBook(String name, String company, int number) {
+		super();
+		this.name = name;
+		this.company = company;
+		this.number = number;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PhoneBook other = (PhoneBook) obj;
+		if (company == null) {
+			if (other.company != null)
+				return false;
+		} else if (!company.equals(other.company))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (number != other.number)
+			return false;
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((company == null) ? 0 : company.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + number;
+		return result;
+	}
+
+	
+
+}
